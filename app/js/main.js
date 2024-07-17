49dc2545cc6bd880ae1fa338a7a8bb352ed1fc4e
Anna Danilova <agdanilova@mail.ru>
2024-07-17 21:03:50 +0300

    Добавлено прокручивание к началу страницы

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+//Прокрутка к началу страницы
+const upButton = document.querySelector('.up-button');
+upButton.addEventListener('click', function () {
+  window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+});
