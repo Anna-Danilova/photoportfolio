0a6e73450838c22f583da004266958be6ef5742f
Anna Danilova <agdanilova@mail.ru>
2024-07-23 16:45:52 +0300

    Имплементация появления кнопки при скролле

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,16 @@ const upButton = document.querySelector('.up-button');
 upButton.addEventListener('click', function () {
   window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
 });
+
+//Появление кнопки ВВЕРХ при прокрутке
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      upButton.classList.add('up-button--visible');
+    } else {
+      upButton.classList.remove('up-button--visible');
+    }
+  });
+});
+observer.observe(document.body);
