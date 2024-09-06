b6ba847cc52b9f07599d19e7a85568fe1bc199ac
Anna Danilova <agdanilova@mail.ru>
2024-09-06 09:43:30 +0300

    Имплементация функционала слайдера

--- app/js/main.js
+++ app/js/main.js
@@ -39,3 +39,36 @@ appearingSections.forEach(function (section) {
   section.classList.add('appearing-section--hide');
   appearanceObserver.observe(section);
 });
+
+//Слайдер
+const slides = document.querySelector('.slider__list');
+const slidesSum = slides.children.length;
+const nextSlideBtn = document.querySelector('.slider__button--next');
+const previousSlideBtn = document.querySelector('.slider__button--prev');
+let currentSlide = 0;
+
+// console.log(slidesSum);
+
+function moveToSlide(slide) {
+  slides.style.transform = `translateX(${-slide * 100}%)`;
+}
+
+moveToSlide(0);
+
+nextSlideBtn.addEventListener('click', function () {
+  if (currentSlide === slidesSum - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  moveToSlide(currentSlide);
+});
+
+previousSlideBtn.addEventListener('click', function () {
+  if (currentSlide === 0) {
+    currentSlide = slidesSum - 1;
+  } else {
+    currentSlide--;
+  }
+  moveToSlide(currentSlide);
+});
