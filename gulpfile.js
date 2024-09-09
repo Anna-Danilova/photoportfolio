e7e4d52046eedc2f8c623a1f784cb7cb47be735a
Anna Danilova <agdanilova@mail.ru>
2024-09-09 17:44:17 +0300

    Улучшено отображение слайдов. Добавлены форматы .avif и .webp

--- gulpfile.js
+++ gulpfile.js
@@ -15,30 +15,33 @@ const newer = require('gulp-newer');
 const svgSprite = require('gulp-svg-sprite');
 const include = require('gulp-include');
 
-
 function pages() {
   return src('app/pages/*.html')
-    .pipe(include({
-      includePaths: 'app/components'
-    }))
+    .pipe(
+      include({
+        includePaths: 'app/components',
+      })
+    )
     .pipe(dest('app'))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
 }
 
 function fonts() {
   return src('app/fonts/src/*.*')
-    .pipe(fonter({
-      formats: ['woff', 'ttf']
-    }))
+    .pipe(
+      fonter({
+        formats: ['woff', 'ttf'],
+      })
+    )
     .pipe(src('app/fonts/*.ttf'))
     .pipe(ttf2woff2())
-    .pipe(dest('app/fonts'))
+    .pipe(dest('app/fonts'));
 }
 
 function images() {
   return src(['app/images/src/*.*', '!app/images/src/*.svg'])
     .pipe(newer('app/images'))
-    .pipe(avif({ quality: 50 }))
+    .pipe(avif({ quality: 80 }))
 
     .pipe(src('app/images/src/*.*'))
     .pipe(newer('app/images'))
@@ -48,32 +51,34 @@ function images() {
     .pipe(newer('app/images'))
     .pipe(imagemin())
 
-    .pipe(dest('app/images'))
+    .pipe(dest('app/images'));
 }
 
 function sprite() {
   return src('app/images/dist/*.svg')
-    .pipe(svgSprite({
-      mode: {
-        stack: {
-          sprite: '../sprite.svg',
-          example: true
-        }
-      }
-    }))
-    .pipe(dest('app/images/dist'))
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg',
+            example: true,
+          },
+        },
+      })
+    )
+    .pipe(dest('app/images/dist'));
 }
 
 function scripts() {
   return src([
-    'app/js/main.js'
+    'app/js/main.js',
     // 'app/js/*.js',
     // '!app/js/main.min.js'
   ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(dest('app/js'))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
 }
 
 function styles() {
@@ -82,36 +87,37 @@ function styles() {
     .pipe(concat('style.min.css'))
     .pipe(scss({ outputStyle: 'compressed' }))
     .pipe(dest('app/css'))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
 }
 
 function watching() {
   browserSync.init({
     server: {
-      baseDir: "app/"
-    }
+      baseDir: 'app/',
+    },
   });
-  watch(['app/scss/**/*.scss'], styles)
-  watch(['app/images/src'], images)
-  watch(['app/js/main.js'], scripts)
-  watch(['app/components/*', 'app/pages/*'], pages)
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/images/src'], images);
+  watch(['app/js/main.js'], scripts);
+  watch(['app/components/*', 'app/pages/*'], pages);
   watch(['app/*.html']).on('change', browserSync.reload);
 }
 
 function cleanDist() {
-  return src(['dist'])
-    .pipe(clean())
+  return src(['dist']).pipe(clean());
 }
 
 function building() {
-  return src([
-    'app/css/style.min.css',
-    'app/images/dist/*.*',
-    'app/fonts/*.*',
-    'app/js/main.min.js',
-    'app/*.html'
-  ], { base: 'app' })
-    .pipe(dest('dist'))
+  return src(
+    [
+      'app/css/style.min.css',
+      'app/images/dist/*.*',
+      'app/fonts/*.*',
+      'app/js/main.min.js',
+      'app/*.html',
+    ],
+    { base: 'app' }
+  ).pipe(dest('dist'));
 }
 
 exports.styles = styles;
